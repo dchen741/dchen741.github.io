e44c0ee4bac3125656df76e0c55cd4041ee9df60
Dennis Chen <dennischen@us.ibm.com>
2016-08-12 14:44:36 -0400

    Made changes to text

--- app/js/components/App.js
+++ app/js/components/App.js
@@ -9,12 +9,12 @@ export default class App extends React.Component {
       <Header />
         <h3 className="standard-text">
           Hello! I graduated in 2015 from <a href="http://www.tufts.edu/">Tufts University</a> where I studied Computer Science.
-          I have taken a variety of CS courses during my time at college and I am fascinated by what is still out there.
-          I love learning about new technologies and enjoy new experiences.
+          I took a variety of CS courses during my time at school and I am fascinated by the ever changing industry.
+          I love learning about different technologies and enjoy new experiences.
           I am based in the Boston area and if you would like to contact me, feel free to <a href="mailto:DennisChen@us.ibm.com">email</a> me!
         </h3>
         <h6 className="text-center standard-text">
-          <i>In my free time, I really like to play with new technologies, meet new people and bake scrumptious treats.</i>
+          <i>In my free time, I really like to meet new people, bake scrumptious treats, and go on adventures.</i>
         </h6>
       </div>
     )
