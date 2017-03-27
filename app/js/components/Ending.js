05b21bb62720c6aa86b5671124fba0c3931bfd5d
Dennis Chen <dennischen@us.ibm.com>
2017-03-27 00:24:25 -0400

    initial finish maybe?

--- app/js/components/Ending.js
+++ app/js/components/Ending.js
@@ -3,6 +3,8 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import Transition from './Transition';
 import Sample from '_media/sample.png';
 import Adventure from './Adventure';
+import Gmail from '_media/gmail-icon.png';
+import LinkedIn from '_media/linkedin-256.png';
 
 export default class Ending extends React.Component {
 
@@ -10,16 +12,28 @@ export default class Ending extends React.Component {
     return(
       <Row>
         <Col xs={12}>
-          <Transition text={"Let's get coffee!"} />
+          <Transition text={"Let's chat!"} />
           <Row>
             <Col xs={8} xsOffset={2}>
               <Row>
-                <Col xs={12} md={3}>
-                  Hit me up!
+                <Col xs={12}>
+                  <div className="dc-text-section-text">
+                    So coffee isn&#039;t really my thing, but I&#039;m totally down for some tea or baked goods!
+                    Hit me up! :)
+                  </div>
+                  <div>
+                    <img className="dc-contact-icons" src={Gmail}></img>
+                    <span><a href="mailto:dchen741@gmail.com" target="_top">Gmail</a></span>
+                  </div>
+                  <div>
+                    <img className="dc-contact-icons" src={LinkedIn}></img>
+                    <span><a href="https://www.linkedin.com/in/dennis-chen-6694787b" target="_blank">LinkedIn</a></span>
+                  </div>
                 </Col>
               </Row>
             </Col>
           </Row>
+          <div className="dc-bottom-spacing"></div>
         </Col>
       </Row>
     )
--- app/js/components/Life.js
+++ app/js/components/Life.js
@@ -14,14 +14,21 @@ export default class Life extends React.Component {
           <Row>
             <Col xs={8} xsOffset={2}>
               <Row>
+                <Col xs={12}>
+                  <div className="dc-text-section-text dc-bottom-spacing">
+                    In my spare time, I like to...
+                  </div>
+                </Col>
+              </Row>
+              <Row>
                 <Col xs={12} md={3}>
                   <Adventure img={Sample} title={'Hawaii (01/2017)'} text={'WOOOO BEACHES'} />
                 </Col>
                 <Col xs={12} md={3}>
-                  <Adventure img={Sample} title={'Japan (10/2016)'} text={'Sushi galore!'} />
+                  <Adventure img={Sample} title={'Franconia Ridge (11/2016)'} text={'A 9 mile snowy hike'} />
                 </Col>
                 <Col xs={12} md={3}>
-                  <Adventure img={Sample} title={'test'} text={'test'} />
+                  <Adventure img={Sample} title={'Japan (10/2016)'} text={'Sushi galore!'} />
                 </Col>
                 <Col xs={12} md={3}>
                   <Adventure img={Sample} title={'test'} text={'test'} />
